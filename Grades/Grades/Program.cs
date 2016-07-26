d567696d9274b26a837cd65509f3214c9253c90b
Danny Robinson <drobinson@healthgrades.com>
2016-07-26 10:23:15 -0600

    Added unit tests

--- Grades/Grades/Program.cs
+++ Grades/Grades/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Speech.Synthesis;
 
 namespace Grades
 {
@@ -10,6 +11,8 @@ namespace Grades
     {
         static void Main(string[] args)
         {
+            //SpeechSynthesizer synth = new SpeechSynthesizer();
+            //synth.Speak("Testing");
             GradeBook book = new GradeBook();
 
             book.AddGrade(91);
