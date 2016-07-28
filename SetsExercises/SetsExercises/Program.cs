4a1e52231e447f872b11e38162a40002cf91b480
Danny Robinson <drobinson@healthgrades.com>
2016-07-28 11:45:36 -0600

    Added Set Examples

---
+++ SetsExercises/SetsExercises/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SetsExercises
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var bigCities = new HashSet<string>
+            {
+                "New York",
+                "Detroit",
+                "Sterling",
+                "Norfolk"
+            };
+
+            string[] citiesInWorld = { "Norfolk", "Detroit", "Manchester" };
+
+            bigCities.Add("Murica");
+            bigCities.Add("Norfolk");
+
+            //UnionWith, IntersectWith, etc
+            bigCities.UnionWith(citiesInWorld);
+            
+            foreach (string city in bigCities)
+                Console.WriteLine(city);
+
+        }
+    }
+}
