54d5d315727ec91dcfb109d90c1d792ceb0b3656
Danny Robinson <drobinson@healthgrades.com>
2016-07-28 11:15:21 -0600

    Sorted list, custom uncased stirng comparer

--- Dictionaries/Dictionaries/Program.cs
+++ Dictionaries/Dictionaries/Program.cs
@@ -10,16 +10,27 @@ namespace Dictionaries
     {
         static void Main(string[] args)
         {
-            var primeMinisters = new Dictionary<string, PrimeMinister>
+            var primeMinisters = new SortedList<string, PrimeMinister>(new UncasedStringComparer())
             {
                 {"JC", new PrimeMinister("James Callaghan", 1976) },
                 {"MT", new PrimeMinister("Margaret Thatcher", 1979) },
                 {"TB",  new PrimeMinister("Tony Blair", 1997) }
             };
 
+            primeMinisters.Add("GB", new PrimeMinister("John Major", 1990));
+
             foreach (var pm in primeMinisters.Values)
                 Console.WriteLine(pm);
             
+
+        }
+    }
+
+    class UncasedStringComparer : IComparer<string>
+    {
+        public int Compare(string x, string y)
+        {
+            return string.Compare(x, y, StringComparison.InvariantCultureIgnoreCase);
         }
     }
 }
