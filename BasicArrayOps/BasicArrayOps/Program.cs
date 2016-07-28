d0dfe6c8807b5cee4452d4d31ef9133e0a92ec67
Danny Robinson <drobinson@healthgrades.com>
2016-07-28 10:02:00 -0600

    Added List exercise

--- BasicArrayOps/BasicArrayOps/Program.cs
+++ BasicArrayOps/BasicArrayOps/Program.cs
@@ -28,18 +28,40 @@ namespace BasicArrayOps
             //    Console.WriteLine(day);
             //}
 
-            int x1;
-            int[] x2;
+            //int x1;
+            //int[] x2;
 
-            int x3 = 5;
-            int[] x4 = new int[5];
+            //int x3 = 5;
+            //int[] x4 = new int[5];
 
-            int[] x5 = new int[5] { 1, 2, 3, 5, 9 };
+            //int[] x5 = new int[5] { 1, 2, 3, 5, 9 };
 
-            foreach (int x in x5)
-            {
-                Console.WriteLine(x);
-            }
+            //foreach (int x in x5)
+            //{
+            //    Console.WriteLine(x);
+            //}
+
+            var presidents = new List<string> {
+                "Jimmy Carter",
+                "Ronald Reagan",
+                "George HW Bush",
+                "Bill Clinton",
+                "George W Bush"
+            };
+            Console.WriteLine("Count = "+presidents.Count);
+            Console.WriteLine("Capacity = "+ presidents.Capacity);
+            presidents.Add("Barack Obama");
+            presidents.Add("Barack Obama2");
+            presidents.Add("Barack Obama3");
+            //Doubles the capacity when it goes over size
+            presidents.Add("Barack Obama4");
+            presidents.Add("Barack Obama5");
+
+            Console.WriteLine("Count = " + presidents.Count);
+            Console.WriteLine("Capacity = " + presidents.Capacity);
+            foreach (string president in presidents)
+                Console.WriteLine(president);
+            
 
 
 
