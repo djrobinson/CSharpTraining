c0b05d713525cabd01f50167e4797422723401ed
Danny Robinson <drobinson@healthgrades.com>
2016-07-28 10:53:16 -0600

    Dictionaries example

---
+++ Dictionaries/Dictionaries/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dictionaries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var primeMinisters = new Dictionary<string, PrimeMinister>
+            {
+                {"JC", new PrimeMinister("James Callaghan", 1976) },
+                {"MT", new PrimeMinister("Margaret Thatcher", 1979) },
+                {"TB",  new PrimeMinister("Tony Blair", 1997) }
+            };
+
+            foreach (var pm in primeMinisters.Values)
+                Console.WriteLine(pm);
+            
+        }
+    }
+}
