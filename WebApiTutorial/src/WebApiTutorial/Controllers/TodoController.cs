eefd4570a2d8534dc41d1a9564a51d817765f2c4
Danny Robinson <drobinson@healthgrades.com>
2016-07-29 22:40:56 -0600

    Completed tutorial

--- WebApiTutorial/src/WebApiTutorial/Controllers/TodoController.cs
+++ WebApiTutorial/src/WebApiTutorial/Controllers/TodoController.cs
@@ -4,7 +4,7 @@ using WebApiTutorial.Models;
 
 namespace WebApiTutorial.Controllers
 {
-    [Route("api/controllers")]
+    [Route("api/[controller]")]
     public class TodoController : Controller
     {
         public TodoController(ITodoRepository todoItems)
@@ -18,7 +18,7 @@ namespace WebApiTutorial.Controllers
             return TodoItems.GetAll();
         }
 
-        [HttpGet("id", Name="GetTodo")]
+        [HttpGet("{id}", Name="GetTodo")]
         public IActionResult GetById(string id)
         {
             var item = TodoItems.Find(id);
@@ -28,5 +28,41 @@ namespace WebApiTutorial.Controllers
             }
             return new ObjectResult(item);
         }
+
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtRoute("GetTodo", new { id = item.Key }, item);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            TodoItems.Remove(id);
+        }
+
     }
 }
--- WebApiTutorial/src/WebApiTutorial/Startup.cs
+++ WebApiTutorial/src/WebApiTutorial/Startup.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using WebApiTutorial.Models;
 
 namespace WebApiTutorial
 {
@@ -39,7 +40,7 @@ namespace WebApiTutorial
 
             services.AddMvc();
 
-            services.AddSingleton<ITodoRepository, TodoRepository()>();
+            services.AddSingleton<ITodoRepository, TodoRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline
