41bcb69893a9206c47f9a773d3a5d635f2ca2b62
Danny Robinson <drobinson@healthgrades.com>
2016-08-02 10:55:37 -0600

    Added search on index page

--- MvcMovie2/src/MvcMovie2/Models/Movie.cs
+++ MvcMovie2/src/MvcMovie2/Models/Movie.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace MvcMovie2.Models
 {
@@ -10,8 +7,9 @@ namespace MvcMovie2.Models
     {
         public int ID { get; set; }
         public string Title { get; set; }
+        [Display(Name = "Release Date")]
+        [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
-        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public string Genre { get; set; }
         public decimal Price { get; set; }
     }
