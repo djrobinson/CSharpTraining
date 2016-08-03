cf2ff9adb5da0e6e20857ae80b42f76fc5427399
Danny Robinson <drobinson@healthgrades.com>
2016-08-03 15:13:54 -0600

    Extensibility tutorial - Interfaces

---
+++ Extensibility/PeopleViewer/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using PersonRepository.Interface;
+using System.Windows;
+using PersonRepository.Service;
+
+namespace PeopleViewer
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ServiceFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+
+            IPersonRepository repository = new ServiceRepository();
+            var people = repository.GetPeople();
+            foreach (var person in people)
+                PersonListBox.Items.Add(person);
+
+            ShowRepositoryType(repository);
+        }
+
+        private void CSVFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void SQLFetchButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void ClearButton_Click(object sender, RoutedEventArgs e)
+        {
+            ClearListBox();
+        }
+
+        private void ClearListBox()
+        {
+            PersonListBox.Items.Clear();
+        }
+
+        private void ShowRepositoryType(IPersonRepository repository)
+        {
+            MessageBox.Show(string.Format("Repository Type:\n{0}",
+                repository.GetType().ToString()));
+        }
+    }
+}
