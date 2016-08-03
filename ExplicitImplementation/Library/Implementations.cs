fa094f7e96195c7e9857ce74ddafb084ad4113e3
Danny Robinson <drobinson@healthgrades.com>
2016-08-03 16:35:57 -0600

    Explicit Interfaces

---
+++ ExplicitImplementation/Library/Implementations.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Library
+{
+    public class StandardCatalog : ISaveable, IPersistable
+    {
+        public void Load()
+        {
+        }
+
+        public string Save()
+        {
+            return "Catalog Save";
+        }
+    }
+
+    public class ExplicitCatalog : ISaveable, IPersistable
+    {
+        public string Save()
+        {
+            return "Catalog Save";
+        }
+
+        string ISaveable.Save()
+        {
+            return "ISaveable Save";
+        }
+
+        string IPersistable.Save()
+        {
+            return "IPersistable Save";
+        }
+    }
+
+    public class EnumerableCatalog : IEnumerable<string>
+    {
+        public IEnumerator<string> GetEnumerator()
+        {
+            return null;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+    }
+
+}
