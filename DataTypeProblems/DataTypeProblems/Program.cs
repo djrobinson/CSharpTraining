630a2c9a63419e89cb3228f3550240bb7b0077a3
Danny Robinson <drobinson@healthgrades.com>
2016-08-16 16:13:46 -0600

    C# data type exercises

---
+++ DataTypeProblems/DataTypeProblems/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataTypeProblems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputValue;
+            char[] brokenValue;
+            Console.WriteLine("Enter 3 letters");
+            inputValue = Console.ReadLine();
+            brokenValue = inputValue.ToCharArray();
+            for (var i = brokenValue.Length - 1; i >= 0; i--)
+            {
+                Console.WriteLine(brokenValue[i]);
+            }
+        }
+    }
+}
