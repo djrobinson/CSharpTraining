a8bc8936120089d20094cb42f6206404c6548eb5
Danny Robinson <drobinson@healthgrades.com>
2016-08-17 11:48:26 -0600

    Do while exercise

--- DataTypeProblems/DataTypeProblems/Program.cs
+++ DataTypeProblems/DataTypeProblems/Program.cs
@@ -10,47 +10,29 @@ namespace DataTypeProblems
     {
         static void Main(string[] args)
         {
-            //string inputValue;
-            //char[] brokenValue;
-            //Console.WriteLine("Enter 3 letters");
-            //inputValue = Console.ReadLine();
-            //brokenValue = inputValue.ToCharArray();
-            //for (var i = brokenValue.Length - 1; i >= 0; i--)
-            //{
-            //    Console.WriteLine(brokenValue[i]);
-            //}
+            int attempts = 0;
+            string username = "Danny";
+            string password = "password";
 
-            int printNumber;
-            int width;
-            ArrayList numList = new ArrayList();
-
-            Console.WriteLine("Enter a number: ");
-            printNumber = Int32.Parse(Console.ReadLine());
-            Console.WriteLine("Enter a width: ");
-            width = Int32.Parse(Console.ReadLine());
-            for (var i = 0; i < width; i++)
+            do
             {
-                numList.Add(printNumber);
-            }
-            RunProgram(numList);
-        }
+                Console.WriteLine("Username: ");
+                string usernameAttempt = Console.ReadLine();
+                Console.WriteLine("Password: ");
+                string passwordAttempt = Console.ReadLine();
 
-        public static void RunProgram(ArrayList numList)
-        {
-            var originalCount = numList.Count;
-            for (var i = 0; i < originalCount; i++)
-            {
-                PrintAllVals(numList);
-                numList.RemoveAt(numList.Count - 1);
-            }
-        }
+                if (username != usernameAttempt || password != passwordAttempt)
+                {
+                    attempts++;
+                }
+                else
+                {
+                    Console.WriteLine("You're logged in-ish");
+                    attempts = 3;
+                }
 
+            } while (attempts < 3);
 
-        public static void PrintAllVals(IEnumerable currList)
-        {
-            foreach (int i in currList)
-                Console.Write(i);
-            Console.WriteLine();
         }
     }
 }
