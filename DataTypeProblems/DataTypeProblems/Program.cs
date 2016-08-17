37008ca82a60a6f3ce3dcedafdae9c2893628a21
Danny Robinson <drobinson@healthgrades.com>
2016-08-17 11:16:19 -0600

    Arraylist practice

--- DataTypeProblems/DataTypeProblems/Program.cs
+++ DataTypeProblems/DataTypeProblems/Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Collections;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,15 +10,47 @@ namespace DataTypeProblems
     {
         static void Main(string[] args)
         {
-            string inputValue;
-            char[] brokenValue;
-            Console.WriteLine("Enter 3 letters");
-            inputValue = Console.ReadLine();
-            brokenValue = inputValue.ToCharArray();
-            for (var i = brokenValue.Length - 1; i >= 0; i--)
+            //string inputValue;
+            //char[] brokenValue;
+            //Console.WriteLine("Enter 3 letters");
+            //inputValue = Console.ReadLine();
+            //brokenValue = inputValue.ToCharArray();
+            //for (var i = brokenValue.Length - 1; i >= 0; i--)
+            //{
+            //    Console.WriteLine(brokenValue[i]);
+            //}
+
+            int printNumber;
+            int width;
+            ArrayList numList = new ArrayList();
+
+            Console.WriteLine("Enter a number: ");
+            printNumber = Int32.Parse(Console.ReadLine());
+            Console.WriteLine("Enter a width: ");
+            width = Int32.Parse(Console.ReadLine());
+            for (var i = 0; i < width; i++)
             {
-                Console.WriteLine(brokenValue[i]);
+                numList.Add(printNumber);
             }
+            RunProgram(numList);
+        }
+
+        public static void RunProgram(ArrayList numList)
+        {
+            var originalCount = numList.Count;
+            for (var i = 0; i < originalCount; i++)
+            {
+                PrintAllVals(numList);
+                numList.RemoveAt(numList.Count - 1);
+            }
+        }
+
+
+        public static void PrintAllVals(IEnumerable currList)
+        {
+            foreach (int i in currList)
+                Console.Write(i);
+            Console.WriteLine();
         }
     }
 }
