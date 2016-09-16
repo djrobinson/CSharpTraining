a4e57c0fe31c1d24a23c9a50ff0412f44dcf04b7
Danny Robinson <drobinson@healthgrades.com>
2016-09-16 13:28:26 -0600

    Getting flow control exercises setup'

---
+++ PracticeExercises/FlowControl/FlowControl/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlowControl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Enter a number");
+            number = Convert.ToInt32(Console.ReadLine());
+            if (number > 0)
+            {
+                Console.WriteLine("Number is positive");
+            } else
+            {
+                Console.WriteLine("Number is negative");
+            }
+        }
+    }
+}
